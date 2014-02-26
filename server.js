a7d6cf1f1ab6a7e4aba18c98fa1f71f5afc39e97
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-02-25 16:01:20 -0800

    debug

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var configs = require('./configs');
 var pubsub = require('redis').createClient(configs.redisPort, configs.redisHost);
+console.log("anand create redis");
 var events = new (require('events').EventEmitter)();
 var Primus = require('primus');
 var http = require('http');
@@ -9,20 +10,25 @@ var primus = new Primus(server, { transformer: 'engine.io' });
 primus.use('substream', require('substream'));
 
 primus.on('connection', function (spark) {
+  console.log("anand connection: ");
+
   var subscribeSpark = spark.substream('subscriptions');
   subscribeSpark.on('data', function (key) {
     console.log('SUB', key);
     var eventSpark = spark.substream(key);
     events.on('events:' + key, onEvent);
     spark.on('end', function () {
+      console.log("anand UNSUB end: "+key);
       events.removeListener('events:' + key, onEvent);
       eventSpark.end();
     });
     function onEvent (data) {
+      console.log("anand onEvent: "+data);
       eventSpark.write(data);
     }
   });
   spark.on('end', function () {
+    console.log("anand end: ");
     subscribeSpark.end();
   });
 });
